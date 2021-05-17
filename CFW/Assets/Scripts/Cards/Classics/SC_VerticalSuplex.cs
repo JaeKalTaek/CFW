25a951f718f7cb05fdfb96e2e6a6220524bede25
Divi <benjaminr@laposte.net>
2021-05-17 16:51:34 +0200

    Vertical Suplex card

---
+++ CFW/Assets/Scripts/Cards/Classics/SC_VerticalSuplex.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using static SC_Player;
+
+namespace Card {
+
+    public class SC_VerticalSuplex : SC_OffensiveMove {
+
+        protected override IEnumerator MakeChoices () {
+
+            yield return StartCoroutine (base.MakeChoices ());
+
+            yield return StartCoroutine (MakeChoice (() => {
+
+                if (localPlayer.Stamina > cost.stamina)
+                    UI.ShowNumberChoiceUI (Mathf.Min (3, localPlayer.Stamina - cost.stamina));
+                else {
+
+                    localPlayer.SetIntChoiceServerRpc ("NumberChoice", 0);
+
+                    MakingChoices = false;
+
+                }
+
+            }));
+
+        }
+
+        protected override IEnumerator ApplyEffects () {
+
+            yield return StartCoroutine (base.ApplyEffects ());
+
+            if (Caller.GetIntChoice ("NumberChoice") != 0) {
+
+                Caller.ApplySingleEffect ("Stamina", -Caller.GetIntChoice ("NumberChoice"));
+
+                Receiver.ApplySingleEffect ("Stamina", -Caller.GetIntChoice ("NumberChoice"));
+
+            }
+
+        }
+
+    }
+
+}
