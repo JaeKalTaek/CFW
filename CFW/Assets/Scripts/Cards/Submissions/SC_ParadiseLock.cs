ea67171a2712bad02b8d82cb9c1230309673aed0
Divi <benjaminr@laposte.net>
2021-05-19 21:07:43 +0200

    Paradise Lock card

---
+++ CFW/Assets/Scripts/Cards/Submissions/SC_ParadiseLock.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using static SC_Player;
+
+namespace Card {
+
+    public class SC_ParadiseLock : SC_Submission {
+
+        public OnCardHovered cardHovered;
+
+        protected override IEnumerator ApplyEffects () {
+
+            yield return StartCoroutine (base.ApplyEffects ());
+
+            boostingCard = this;
+
+            cardHovered = new OnCardHovered ((c, b) => {
+
+                if (localPlayer.Turn && localPlayer.Unlocked && c.Is (SC_Global.CardType.Strike)) {
+
+                    if (b)
+                        c.commonEffects.Add (new CommonEffect (CommonEffectType.Break));
+                    else
+                        c.commonEffects.RemoveAt (c.commonEffects.Count - 1);
+
+                }
+
+            });
+
+            OnCardHoveredEvent += cardHovered;
+
+        }
+
+        public override void ApplyBoosts () {            
+
+            if (activeCard.Is (SC_Global.CardType.Strike)) {
+
+                activeCard.commonEffects.Add (new CommonEffect (CommonEffectType.Break));
+
+                (activeCard as SC_OffensiveMove).effectOnOpponent.stamina += 1;
+
+                base.ApplyBoosts ();
+
+            }
+
+        }
+
+        public override void Broken () {
+
+            base.Broken ();
+
+            boostingCard = boostingCard == this ? null : boostingCard;
+
+            OnCardHoveredEvent -= cardHovered;
+
+        }
+
+    }
+
+}
+
