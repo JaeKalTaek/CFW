76ab9cd7f4fb022b110ebb6277fb514287b30b74
Divi <benjaminr@laposte.net>
2021-05-22 23:16:25 +0200

    Hulk Out card

---
+++ CFW/Assets/Scripts/Cards/Specials/SC_HulkOut.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using Card;
+
+public class SC_HulkOut : SC_BaseCard {
+
+    protected override IEnumerator ApplyEffects () {
+
+        modifierCards.Add (this);
+
+        yield return StartCoroutine (base.ApplyEffects ());        
+
+    }
+
+    public override void ApplyModifiers () {
+
+        SC_OffensiveMove c = activeCard as SC_OffensiveMove;
+
+        c.effectOnOpponent.bodyPartDamage.damage /= 2;
+        c.effectOnOpponent.health /= 2;
+        c.effectOnOpponent.stamina /= 2;
+
+        base.ApplyModifiers ();
+
+    }
+
+}
